962b321d376a593a9ba1dc7313a21a819c973907
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2022-01-09 11:25:44 +0100

    refactore the project

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
+import { Router } from 'express'
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+export const usersRouter = Router()
 
-module.exports = router;
+usersRouter.get('/', function(req, res, next) {
+  res.send('respond with a resource')
+})
