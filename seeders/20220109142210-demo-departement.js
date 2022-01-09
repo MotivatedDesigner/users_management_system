0ef36be19c03054107829297401745388b7148ad
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2022-01-09 17:05:07 +0100

    adding departements seeders

--- seeders/20220109142210-demo-departement.js
+++ seeders/20220109142210-demo-departement.js
@@ -2,12 +2,32 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('departements', [{
+    return queryInterface.bulkInsert('departements', [
+    {
       name: 'physics',
       description: 'lal lal lala',
       createdAt: new Date(),
       updatedAt: new Date()
-    }]);
+    },
+    {
+      name: 'chemistry',
+      description: 'lal lal lala',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'math',
+      description: 'lal lal lala',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'biology',
+      description: 'lal lal lala',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+  ]);
   },
 
   down: async (queryInterface, Sequelize) => {
